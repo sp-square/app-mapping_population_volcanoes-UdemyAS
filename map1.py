ea9e0e1de9d83f5fd5dc980f9d56267744278ee6
Sandrine Poissonnet <spoisson@gmail.com>
2022-01-05 00:14:46 -0800

    adding html on popups

--- map1.py
+++ map1.py
@@ -5,6 +5,8 @@ import pandas
 data = pandas.read_csv('Volcanoes.txt')
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
+name = list(data["NAME"])
 
 # create map instance with specific parameters
 map = folium.Map(location=[33.378, -118.426],
@@ -13,9 +15,11 @@ map = folium.Map(location=[33.378, -118.426],
 # add features to the map
 fg = folium.FeatureGroup(name='My Map')
 
-for lt, ln in zip(lat, lon):
+for lt, ln, el, name in zip(lat, lon, elev, name):
+    iframe = folium.IFrame(
+        html=f'Volcano name: <a href="https://www.google.com/search?q={name}" target="_blank">{name}</a><br>Height: {el} ft', width=200, height=100)
     fg.add_child(folium.Marker(
-        location=[lt, ln], popup='Volcano', icon=folium.Icon(color='green')))
+        location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
 
 # add feature group to the map
 map.add_child(fg)
