053eb9dc3d306f4d5a5b86563a41ae61c7cf16dd
Sandrine Poissonnet <spoisson@gmail.com>
2022-01-05 00:24:02 -0800

    add a color generation function for the markers

--- map1.py
+++ map1.py
@@ -8,6 +8,17 @@ lon = list(data["LON"])
 elev = list(data["ELEV"])
 name = list(data["NAME"])
 
+
+# function to produce the marker's color depending on the elevation
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
 # create map instance with specific parameters
 map = folium.Map(location=[33.378, -118.426],
                  zoom_start=8, tiles='Stamen Terrain')
@@ -19,7 +30,7 @@ for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(
         html=f'Volcano name: <a href="https://www.google.com/search?q={name}" target="_blank">{name}</a><br>Height: {el} ft', width=200, height=100)
     fg.add_child(folium.Marker(
-        location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
+        location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(el))))
 
 # add feature group to the map
 map.add_child(fg)
