415e9d73ac0bcb8dd33f02b83f49b7c779ad9dc5
Sandrine Poissonnet <spoisson@gmail.com>
2022-01-06 15:25:52 -0800

    stylize circle markers

--- map1.py
+++ map1.py
@@ -21,7 +21,7 @@ def color_producer(elevation):
 
 # create map instance with specific parameters
 map = folium.Map(location=[33.378, -118.426],
-                 zoom_start=8, tiles='Stamen Terrain')
+                 zoom_start=6, tiles='Stamen Terrain')
 
 # add features to the map
 fg = folium.FeatureGroup(name='My Map')
@@ -29,8 +29,10 @@ fg = folium.FeatureGroup(name='My Map')
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(
         html=f'Volcano name: <a href="https://www.google.com/search?q={name}" target="_blank">{name}</a><br>Height: {el} ft', width=200, height=100)
-    fg.add_child(folium.Marker(
-        location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(el))))
+    # fg.add_child(folium.Marker(
+    #     location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(el))))
+    fg.add_child(folium.CircleMarker(radius=10,
+                                     location=[lt, ln], popup=folium.Popup(iframe), color='grey', fill_color=color_producer(el), fill_opacity=0.7))
 
 # add feature group to the map
 map.add_child(fg)
